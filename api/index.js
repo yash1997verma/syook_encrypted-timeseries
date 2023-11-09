a36028c02d3d98c282db5cb9dc5ab726c0b8e6ea
Yash Verma <yash1997verma@gmail.com>
2023-11-09 18:05:17 +0530

    emitter setup

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ io.on('connection', (socket)=>{
     })
 })
 
-//for genrating random message
+//for genrating random message with secret_key attached
 const randomMessage = ()=>{
     const randomNameIndex = Math.floor(Math.random() * data.names.length);
     const randomOriginIndex = Math.floor(Math.random() * data.cities.length);
@@ -41,14 +41,54 @@ const randomMessage = ()=>{
         destination: data.cities[randomDestinationIndex]
     };
 
-    return originalMessage;
+    // calculate SHA-256 hash of the original message
+    const secretKey = crypto.createHash('sha256').update(JSON.stringify(originalMessage)).digest('hex');
+
+    // add secret key to the original message
+    const sumCheckMessage = {
+        ...originalMessage,
+        secret_key: secretKey,
+    };
+
+    const messageString = JSON.stringify(sumCheckMessage);
+
+    return messageString;
+
+}
+
+const messageKey = crypto.randomBytes(32);
+
+//genrate a encrypted stream from collection of messages
+const encryptedStream = ()=>{
+    //any no. between 49-499
+    const numMessages = Math.floor(Math.random() * (499 - 49 + 1) + 49); 
+    const messages = [];
+
+    for(let i = 0; i<numMessages; i++){
+        const payloadString = randomMessage();
+        const key = messageKey; 
+
+        //create and aes-256-ctr cipher with the passphrase
+        const cipher = crypto.createCipheriv('aes-256-ctr', Buffer.from(key), crypto.randomBytes(16));
+        
+        //encrypt the payload 
+        let encryptedMessage = cipher.update(payloadString, 'utf-8', 'hex');
+        encryptedMessage += cipher.final('hex');
+        messages.push(encryptedMessage);
+    }
+
+    // Concatenate the encrypted messages into a single string separated by '|'
+    const stream = messages.join('|');
+    
+    return stream;
 }
 
 
 //emit a random message over the WebSocket connection
 const emitRandomMessage = () => {
-    const message = randomMessage();
-    io.emit('dataStream', JSON.stringify(message));
+    const stream = encryptedStream();
+    // console.log(stream);
+    io.emit('dataStream', JSON.stringify(stream));
 };
 
 
@@ -57,7 +97,7 @@ const emitRandomMessage = () => {
 //emmit the encrypted stream every 10 sec
 setInterval(()=>{
     emitRandomMessage();
-},1000);
+},10000);
 
 
 
