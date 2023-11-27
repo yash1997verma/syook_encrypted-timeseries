0727029e3657f3b5bd234ca1ec21c735e992542b
Yash Verma <yash1997verma@gmail.com>
2023-11-27 09:57:38 +0530

    api

--- api/config/mongoose.js
+++ api/config/mongoose.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
+
 require('dotenv').config();
 
 async function main(){
     try{
         await mongoose.connect(process.env.MONGO_URL);
-        console.log(`connected to db`);
+        console.log('connected to DB');
     }catch(err){
-        console.log(`error in connecting to db ${err}`);
+        console.log(`error in connecting to DB ${err}`);
     }
 }
-
 
 main();
--- api/index.js
+++ api/index.js
@@ -1,57 +1,30 @@
 const express = require('express');
-const http = require('http');
-const crypto = require('crypto');
-const data = require('./data.json');
-const socketIO = require('socket.io');
+const app = express ();
+const port = 8000;
+const passport = require('passport');
+
+//cors
 const cors = require('cors');
+//setting up cors middleware
+app.use(cors());
+
+//setting up passport middleware
+app.use(passport.initialize());
+// app.use(passport.session());
 
 //setting up db
 const db = require('./config/mongoose');
 
-//import emitter and listner
-const emitter = require('./services/emitter')
-const listener = require('./services/listener')
+//middleware to parse json and url encoded data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+//use express router
+app.use('/', require('./routes'));
 
-// http server
-const app = express();
-const server = http.createServer(app);
-const port = 80;
-
-//setup socket server using http server
-const io = new socketIO.Server(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
+app.listen(port, (err)=>{
+    if(err){
+        console.log(`error in starting server ${err}`);
     }
+    console.log(`Server is running on ${port}`);
 })
-
-//setup socket.io
-io.on('connection', (socket)=>{
-    console.log(`Client connected on ${port}`);
-
-    //setup listener
-    listener.setupListener(socket)
-    
-    //handle disconnection
-    socket.on('disconnected', ()=>{
-        console.log('client disconnected');
-    })
-});
-
-//setup emitter
-emitter.setupEmitter(io);
-
-server.listen(port, ()=>{
-    try{
-        console.log(`Server is running on port : ${port}`)
-    }catch(err){
-        console.log(`Error in starting server ${err}`);
-    }
-    
-});
-
-
-
-
-
